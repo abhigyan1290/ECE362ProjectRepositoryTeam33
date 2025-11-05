1d5c1253ef2156ed4988d16ed9629f82f56fc379
Abhigyan Doshi <abhigyandoshi1@gmail.com>
2025-11-05 14:33:44 -0500

    Step 1 - LED Demo

---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include "pico/stdlib.h"
+
+// Change these if your wiring differs
+#define LED_R 37
+#define LED_G 38
+#define LED_B 39
+
+// Assume COMMON-ANODE RGB (active LOW). If yours is common-cathode, flip ON/OFF macros.
+#define LED_ON(pin)  gpio_put(pin, 0)
+#define LED_OFF(pin) gpio_put(pin, 1)
+
+int main(void) {
+    stdio_init_all();
+
+    gpio_init(LED_R); gpio_set_dir(LED_R, GPIO_OUT);
+    gpio_init(LED_G); gpio_set_dir(LED_G, GPIO_OUT);
+    gpio_init(LED_B); gpio_set_dir(LED_B, GPIO_OUT);
+
+    // All OFF initially
+    LED_OFF(LED_R); LED_OFF(LED_G); LED_OFF(LED_B);
+
+    while (true) {
+        // Red
+        LED_ON(LED_R); LED_OFF(LED_G); LED_OFF(LED_B);
+        sleep_ms(4000);
+
+        // Green
+        LED_OFF(LED_R); LED_ON(LED_G); LED_OFF(LED_B);
+        sleep_ms(4000);
+
+        // Blue
+        LED_OFF(LED_R); LED_OFF(LED_G); LED_ON(LED_B);
+        sleep_ms(4000);
+
+        // All OFF (gap)
+        LED_OFF(LED_R); LED_OFF(LED_G); LED_OFF(LED_B);
+        sleep_ms(3000);
+    }
+}
